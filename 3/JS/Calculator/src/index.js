324cf49cf257c500fd22aad2a837daedbebc3eb7
ryan <ryannesh2376@gmail.com>
2025-02-12 20:12:25 +0300

    calcFinal

--- 3/JS/Calculator/src/index.js
+++ 3/JS/Calculator/src/index.js
@@ -1,8 +1,29 @@
-// document.getElementById("")
+// const currentInput = ""
 const appendNumber = (number) => {
-    if (number == "0" && number !== ".") {
-        document.getElementById("display").value += number;
-    } else {
-        
+    document.getElementById("display").value += number;
+}
+
+const appendOperator = (operator) => {
+    document.getElementById("display").value += operator;
+}
+
+const clearDisplay = () => {
+    document.getElementById("display").value = "";
+}
+
+const calculate = () => {
+    try{
+        document.getElementById("display").value = eval(document.getElementById("display").value);
+    }
+    catch(error){
+        document.getElementById("display").value = "Error"
+    }
+}
+
+const backSpace = () => {
+    let currentInput = document.getElementById("display").value;
+    if(currentInput.length > 0) {
+        currentInput = currentInput.slice(0, -1);
+        document.getElementById("display").value = currentInput;
     }
 }
