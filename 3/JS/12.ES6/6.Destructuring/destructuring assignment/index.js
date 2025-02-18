1f92c4d95b55d0bec6edbc274e7105249e4d9608
ryan <ryannesh2376@gmail.com>
2025-02-18 09:04:32 +0300

    stru

--- 3/JS/12.ES6/6.Destructuring/destructuring assignment/index.js
+++ 3/JS/12.ES6/6.Destructuring/destructuring assignment/index.js
@@ -37,8 +37,13 @@ const user = {
 };
 
 // Destructuring user data
-const { id, name: { first, last }, email, address: { shipping, billing }, payment: { transactions } } = user;
-
+const {id, name: { first, last }, email,
+address: { 
+    shipping: { street, city, state, postalcode, country }, 
+    billing: { street: street2, city: city2, state: state2, postalCode: postalcode2, country: country2 } 
+    },
+    payment: {transactions}
+}= user;
 // Display Personal Information
 document.getElementById('personal-info').innerHTML = `
     <h2>Personal Information</h2>
@@ -50,19 +55,19 @@ document.getElementById('personal-info').innerHTML = `
 // Display Shipping Address
 document.getElementById('shipping-address').innerHTML = `
     <h2>Shipping Address</h2>
-    <p>${shipping.street}, ${shipping.city}, ${shipping.state}, ${shipping.postalCode}, ${shipping.country}</p>
+    <p>${street}, ${city}, ${state}, ${postalcode}, ${country}</p>
 `;
 
 // Display Billing Address
 document.getElementById('billing-address').innerHTML = `
     <h2>Billing Address</h2>
-    <p>${billing.street}, ${billing.city}, ${billing.state}, ${billing.postalCode}, ${billing.country}</p>
+    <p>${street2}, ${city2}, ${state2}, ${postalcode2}, ${country2}</p>
 `;
 
 // Display Transactions List
 const transactionList = document.getElementById('transaction-list');
-transactions.forEach(transaction => {
+transactions.forEach(transactions => {
     const li = document.createElement('li');
-    li.innerHTML = `<strong>${transaction.description}</strong> - $${transaction.amount} (ID: ${transaction.id})`;
+    li.innerHTML = `<strong>${transactions.description}</strong> - $${transactions.amount} (ID: ${transactions.id})`;
     transactionList.appendChild(li);
 });
