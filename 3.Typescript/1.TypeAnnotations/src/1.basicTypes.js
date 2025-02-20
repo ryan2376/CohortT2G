aa87441b78a54c5fce2a576d38ae18d45331f642
ryan <ryannesh2376@gmail.com>
2025-02-20 12:51:07 +0300

    typescript1

---
+++ 3.Typescript/1.TypeAnnotations/src/1.basicTypes.js
@@ -0,0 +1,14 @@
+"use strict";
+// Annotation is giving sth its own type
+// we use : to annotate
+let name1 = "Julia";
+let name2 = "Julia";
+console.log(name2);
+// function parameter annotations 
+const logAlbumInfo = (title, trackCount, isReleased) => {
+    return title;
+};
+// declaring a variable
+let car = "Ferrari";
+let carTyres = 0;
+let example = null;
---
+++ 3.Typescript/1.TypeAnnotations/src/1.basicTypes.ts
@@ -0,0 +1,19 @@
+// Annotation is giving sth its own type
+// we use : to annotate
+
+let name1 = "Julia"
+
+let name2: string = "Julia"
+console.log(name2);
+
+// function parameter annotations 
+
+const logAlbumInfo = (title:string, trackCount:number, isReleased:boolean) => {
+    return title
+}
+
+// declaring a variable
+let car:string = "Ferrari"
+
+let carTyres:number = 0
+let example:null = null
