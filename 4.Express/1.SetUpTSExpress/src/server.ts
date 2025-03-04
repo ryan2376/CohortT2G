9b4f145106118d82030a0483f9b6201df1d8d532
ryan <ryannesh2376@gmail.com>
2025-03-04 10:38:47 +0300

    CORS

--- 4.Express/1.SetUpTSExpress/src/server.ts
+++ 4.Express/1.SetUpTSExpress/src/server.ts
@@ -1,5 +1,8 @@
 import express from "express";
 import dotenv from 'dotenv';
+import { readFileSync } from "fs";
+import path from "path";
+import cors from "cors"
 
 // configure dotenv
 dotenv.config();
@@ -11,9 +14,31 @@ const app = express();
 const port = process.env.PORT
 console.log(port);
 
+// enable cors for all origins
+// app.use(cors())
+
+// enable cors with options
+app.use(cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "PUT", "DELETE"],
+    credentials: true, // allow session cookies
+}))
+
+// get the current directory
+const _dirname = path.resolve()
+
+// synchronously read the file
+const eventData = readFileSync(
+    path.join(_dirname, "src", "db", "eventsData.json"), "utf-8"
+)
+console.log(eventData);
+
 // simple GET request
 app.get('/', (req, res) => {
     res.send('Hello World!');
+})
+app.get('/events', (req, res) => {
+    res.send();
 })
 
 
