1fa004b6bfa5a70bb0fb5f33c82bac01788ede88
ryan <ryannesh2376@gmail.com>
2025-03-13 13:11:48 +0300

    E2E

---
+++ 4.Express/6.E2E-API/src/server.ts
@@ -0,0 +1,44 @@
+// dotenv configure
+// instance of express
+// load all variables
+// enable all middlewares
+// create all routes
+// load more middlewares - error handlers
+// start server
+
+import express from "express";
+import dotenv from 'dotenv'
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
+
+dotenv.config()
+
+
+const app = express();
+
+// NEVER FORGET
+app.use(express.json()) //parses  application/json
+
+// 
+app.use(cookieParser())
+
+app.use(cors({
+    origin: 'http://localhost:5173',
+    methods: "GET, POST, PUT, DELETE",
+    credentials: true
+}))
+
+
+// routes
+app.get('/api/v1/test', (req, res) => {
+    res.send({ message: 'Hello, World!' })
+})
+
+// Middlewares for error handlers
+
+
+// start the server
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+    console.log(`Server is running on port: ${PORT}`)
+}) 
