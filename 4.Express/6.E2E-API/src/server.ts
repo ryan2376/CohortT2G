a600f013fa8d7e14d3026f133c3d8ee52866e826
ryan <ryannesh2376@gmail.com>
2025-03-15 12:34:22 +0300

    pcg

--- 4.Express/6.E2E-API/src/server.ts
+++ 4.Express/6.E2E-API/src/server.ts
@@ -24,7 +24,7 @@ app.use(cookieParser())
 
 app.use(cors({
     origin: 'http://localhost:5173',
-    methods: "GET, POST, PUT, DELETE",
+    methods: "GET, POST, PUT, PATCH, DELETE",
     credentials: true
 }))
 
